4d2cee03399236b9c02dc54a1c3cdeda99ea08d5
jessicmz <jessicmz@uci.edu>
2016-11-04 21:52:45 -0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*
+$(document).ready(function () {
+	$('#header').hide().delay(500).fadeIn(3000);
+	$('.img-circle').hide().delay(600).fadeIn(3000);
+	$('#fade').hide().delay(700).fadeIn(3000);
+
+	$('#main').hide().fadeIn(2000);
+
+
+});
+*/
